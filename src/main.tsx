d18e6f017ee9e0fd047f014a7db86fab7ed7e63b
phantom0z <agnibha0016@gmail.com>
2023-06-01 18:22:49 +0530

    minor fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 
-import { HTML5Backend } from "react-dnd-html5-backend"
-import { DndProvider } from "react-dnd"
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
+    <App />
   </React.StrictMode>
 )
